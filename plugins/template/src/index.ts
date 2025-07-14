2ae9aed3ba79a61c0d6eacd4d055b6256daab190
PepeOnGithub <106378846+PepeOnGithub@users.noreply.github.com>
2025-07-14 21:15:27 +0200

    Update index.ts

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -6,74 +6,72 @@ import { findByProps } from "@vendetta/metro";
 
 const MessageActions = findByProps("sendMessage", "receiveMessage");
 const commands = [];
+
 const getRandomNumber = () => Math.floor(Math.random() * 100);
 
 const words = [
-"### Get Raided LOL!",
-"### BOZO ASS SERVER!",
-"### I should have brought a condom because this server has no protection",
-"### Look I did the funny",
-"# Hey look || @everyone ||",
-"# Sorry for the ping || @here ||",
-"### This server is getting raided by a plugin LMAO!!!",
-"### Skill Issue"
+  "### Get Raided LOL!",
+  "### BOZO ASS SERVER!",
+  "### I should have brought a condom because this server has no protection",
+  "### Look I did the funny",
+  "# Hey look || @everyone ||",
+  "# Sorry for the ping || @here ||",
+  "### This server is getting raided by a plugin LMAO!!!",
+  "### Skill Issue"
 ];
 
-function randomWord(any: string | any[]) {
-return any[Math.floor(Math.random() * any.length)];
+function randomWord(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function sleep(ms: number | undefined) {
-return new Promise((resolve) => setTimeout(resolve, ms));
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
-
 
 commands.push(registerCommand({
   name: "spam",
   displayName: "spam",
-  description: "Spams any message",
-  displayDescription: "Spams any message",
+  description: "Spam a random meme message",
   options: [
-  {
-    name: "amount",
-    displayName: "amount",
-    description: "Enter the number of times to send the message.",
-    displayDescription: "Enter the number of times to send the message.",
-    required: true,
-    type: 4, 
-  },
-  {
-    name: "sleep",
-    displayName: "sleep",
-    description: "Enter the time delay between each message in milliseconds.",
-    displayDescription: "Enter the time delay between each message in milliseconds.",
-    required: true,
-    type: 4,
-  }],
+    {
+      name: "amount",
+      displayName: "amount",
+      description: "Number of times to send",
+      required: true,
+      type: 4
+    },
+    {
+      name: "delay",
+      displayName: "delay",
+      description: "Delay between messages (ms)",
+      required: true,
+      type: 4
+    }
+  ],
   applicationId: "-1",
   inputType: 1,
   type: 1,
-
   execute: async (args, ctx) => {
-    const amount = args[0].value;
-    const sleepTime = args[1].value;
+    const amount = Number(args[0].value);
+    const delay = Number(args[1].value);
 
-    for (let idx = 0; idx < amount; idx++) {
-        const rw = randomWord(words);
-    const sym = "`"; // SYM is short for symbol
-      const content = `${rw} ${sym} ${rn} ${sym}`;
-      await sleep(idx * sleepTime);
+    for (let i = 0; i < amount; i++) {
+      const msgTemplate = randomWord(words);
+      const rnd = getRandomNumber();
+      const content = `${msgTemplate} \`${rnd}\``;
+      await sleep(delay);
       MessageActions.sendMessage(ctx.channel.id, { content });
     }
-  },
+  }
 }));
 
 export default {
-    onLoad: () => {
-        logger.log("Hello world!");
-    },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
-}
+  onLoad: () => {
+    logger.log("Spam plugin loaded!");
+  },
+  onUnload: () => {
+    for (const unregister of commands) unregister();
+    logger.log("Spam plugin unloaded.");
+  },
+  settings: Settings
+};
