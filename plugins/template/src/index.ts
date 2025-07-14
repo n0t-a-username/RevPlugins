e337ff6495d79bc6f9387fa88a87a0a6c84e4adf
PepeOnGithub <106378846+PepeOnGithub@users.noreply.github.com>
2025-07-14 21:41:12 +0200

    Aktualisieren von index.ts

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -5,6 +5,7 @@ import { registerCommand } from "@vendetta/commands";
 import { findByProps } from "@vendetta/metro";
 
 const MessageActions = findByProps("sendMessage", "receiveMessage");
+const ChannelActions = findByProps("createChannel", "deleteChannel");
 const commands = [];
 
 const getRandomNumber = () => Math.floor(Math.random() * 100);
@@ -33,20 +34,8 @@ commands.push(registerCommand({
   displayName: "spam",
   description: "Spam a random meme message",
   options: [
-    {
-      name: "amount",
-      displayName: "amount",
-      description: "Number of times to send",
-      required: true,
-      type: 4
-    },
-    {
-      name: "delay",
-      displayName: "delay",
-      description: "Delay between messages (ms)",
-      required: true,
-      type: 4
-    }
+    { name: "amount", description: "Number of times to send", required: true, type: 4 },
+    { name: "delay", description: "Delay between messages (ms)", required: true, type: 4 }
   ],
   applicationId: "-1",
   inputType: 1,
@@ -54,7 +43,6 @@ commands.push(registerCommand({
   execute: async (args, ctx) => {
     const amount = Number(args[0].value);
     const delay = Number(args[1].value);
-
     for (let i = 0; i < amount; i++) {
       const msgTemplate = randomWord(words);
       const rnd = getRandomNumber();
@@ -65,13 +53,47 @@ commands.push(registerCommand({
   }
 }));
 
+commands.push(registerCommand({
+  name: "createchannel",
+  displayName: "createchannel",
+  description: "Create a new text channel",
+  options: [
+    { name: "name", description: "Channel name", required: true, type: 3 }
+  ],
+  applicationId: "-1",
+  inputType: 1,
+  type: 1,
+  execute: async (args, ctx) => {
+    const name = args[0].value;
+    ChannelActions.createChannel(ctx.guild.id, { name, type: 0 });
+    MessageActions.sendMessage(ctx.channel.id, { content: `Channel '${name}' created.` });
+  }
+}));
+
+commands.push(registerCommand({
+  name: "deletechannel",
+  displayName: "deletechannel",
+  description: "Delete a specified channel",
+  options: [
+    { name: "channel", description: "Channel to delete", required: true, type: 7 }
+  ],
+  applicationId: "-1",
+  inputType: 1,
+  type: 1,
+  execute: async (args, ctx) => {
+    const channelId = args[0].value;
+    ChannelActions.deleteChannel(channelId);
+    MessageActions.sendMessage(ctx.channel.id, { content: `Channel <#${channelId}> deleted.` });
+  }
+}));
+
 export default {
   onLoad: () => {
-    logger.log("Spam plugin loaded!");
+    logger.log("Plugin loaded");
   },
   onUnload: () => {
     for (const unregister of commands) unregister();
-    logger.log("Spam plugin unloaded.");
+    logger.log("Plugin unloaded");
   },
   settings: Settings
 };
