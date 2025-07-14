1ee797bf49ea101e5bb7d3534910987f722ec0f7
Nah <153406659+n0t-a-username@users.noreply.github.com>
2025-07-14 16:15:01 -0400

    Update Settings.tsx

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,40 +1,8 @@
-import { React, ReactNative } from "@vendetta/metro/common";
-import { General } from "@vendetta/ui/components";
-import { useProxy } from "@vendetta/storage";
-import settings from "../settings"; // your plugin settings
+import { Forms } from "@vendetta/ui/components";
+const { FormText } = Forms;
 
-const { View, ScrollView, Text } = ReactNative;
-const { FormRow, FormSwitch, FormDivider } = General;
-
-export default function SettingsPage() {
-  useProxy(settings);
-
-  return (
-    <ScrollView style={{ padding: 16 }}>
-      <Text style={{ fontSize: 24, marginBottom: 16 }}>⚙️ Cool Plugin Settings</Text>
-
-      <FormRow
-        label="Enable Awesome Feature"
-        subLabel="Makes your client 200% more awesome"
-        trailing={
-          <FormSwitch
-            value={settings.awesomeFeature}
-            onValueChange={(v: boolean) => (settings.awesomeFeature = v)}
-          />
-        }
-      />
-
-      <FormDivider />
-
-      <FormRow
-        label="Enable Dark Mode"
-        trailing={
-          <FormSwitch
-            value={settings.darkMode}
-            onValueChange={(v: boolean) => (settings.darkMode = v)}
-          />
-        }
-      />
-    </ScrollView>
-  );
-}
+export default () => (
+    <FormText>
+        Hello, world!
+    </FormText>
+)
