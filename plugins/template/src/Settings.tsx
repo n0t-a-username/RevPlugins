1285193bc57e2c62df435738f55204168ceaaf3d
Nah <153406659+n0t-a-username@users.noreply.github.com>
2026-01-19 07:14:12 -0800

    Update Settings.tsx

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -3,6 +3,6 @@ const { FormText } = Forms;
 
 export default () => (
     <FormText>
-        Hello, world! I am not responsible for whatever happens to your account! v1.0.3
+        Hello, world! I am not responsible for whatever happens to your account! v1.0.4
     </FormText>
 )
