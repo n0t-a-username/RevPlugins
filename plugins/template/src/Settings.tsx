c9492cf4ca06bbaa60b742e61c827b06d882d40a
Nah <153406659+n0t-a-username@users.noreply.github.com>
2026-01-20 17:36:57 -0500

    Update and rename Settings.tsx to Index.tsx

--- plugins/template/src/Settings.tsx
+++
@@ -1,8 +0,0 @@
-import { Forms } from "@vendetta/ui/components";
-const { FormText } = Forms;
-
-export default () => (
-    <FormText>
-        Hello, world! I am not responsible for whatever happens to your account! v1.0.6
-    </FormText>
-)
