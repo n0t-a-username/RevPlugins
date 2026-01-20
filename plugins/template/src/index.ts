bbbd9e7a57d547ae727d60b2db8b5f7b9e655857
Nah <153406659+n0t-a-username@users.noreply.github.com>
2026-01-20 18:31:37 -0500

    Update index.ts

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,2 +1,28 @@
-// index.js
-module.exports = require("./index.tsx");
+import { React } from "@vendetta/metro";
+import { registerCommand, unregisterCommand } from "@vendetta/commands";
+import { showConfirmationModal } from "@vendetta/ui/alerts";
+import CustomUIModal from "./CustomUIModal";
+
+let commandId: string | null = null;
+
+export default {
+  onLoad() {
+    commandId = registerCommand({
+      name: "customui",
+      description: "Open a custom UI modal",
+      options: [],
+      execute() {
+        showConfirmationModal({
+          title: "Custom UI",
+          content: <CustomUIModal />,
+          confirmText: "Close",
+          cancelText: null
+        });
+      }
+    });
+  },
+
+  onUnload() {
+    if (commandId) unregisterCommand(commandId);
+  }
+};
