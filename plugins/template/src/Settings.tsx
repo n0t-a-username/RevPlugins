f5c9ada3f9a65c7c8fdb9b7811fd3eba1364642b
Nah <153406659+n0t-a-username@users.noreply.github.com>
2026-01-27 23:33:21 -0500

    Add Settings component with warning message

---
+++ plugins/template/src/Settings.tsx
@@ -0,0 +1,8 @@
+import { Forms } from "@vendetta/ui/components";
+const { FormText } = Forms;
+
+export default () => (
+    <FormText>
+        Hello, world! I am not responsible for whatever happens to your account!
+    </FormText>
+)
