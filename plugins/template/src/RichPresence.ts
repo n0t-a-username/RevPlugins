deaf06b3df5191e1d618a4d627537ae05ade7cb5
Nah <153406659+n0t-a-username@users.noreply.github.com>
2026-03-02 17:30:23 -0800

    Update RichPresence.ts

--- plugins/template/src/RichPresence.ts
+++ plugins/template/src/RichPresence.ts
@@ -1,16 +1,20 @@
 import { FluxDispatcher } from "@vendetta/metro/common";
+import { findByProps } from "@vendetta/metro";
 import { storage } from "@vendetta/plugin";
 
 const APPLICATION_ID = "1054951789318909972";
+const assetManager = findByProps("getAssetIds");
 
-const activity = {
+const IMAGE_URL = "https://raw.githubusercontent.com/n0t-a-username/RevPlugins/refs/heads/master/plugins/template/src/components/Bemmo.png";
+
+const baseActivity = {
   name: "Streaming",
   application_id: APPLICATION_ID,
   type: 1, // STREAMING
   details: "Bemmo",
   state: "Discord Utility Tool",
   assets: {
-    large_image: "https://raw.githubusercontent.com/n0t-a-username/RevPlugins/refs/heads/master/plugins/template/src/components/Bemmo.png", // must be uploaded in dev portal
+    large_image: IMAGE_URL,
   },
 };
 
@@ -23,12 +27,54 @@ function setActivity(data: any | null) {
   });
 }
 
-export function startRichPresence() {
-  if (storage.hiddenSettings?.enabled) {
-    setActivity(activity);
+let interval: NodeJS.Timeout | null = null;
+let lastState = false;
+
+async function resolveAsset(activity: any) {
+  if (!activity.assets || !activity.assets.large_image) return activity;
+
+  try {
+    const args = [activity.application_id, [activity.assets.large_image]];
+    let assetIds = assetManager.getAssetIds(...args);
+
+    if (!assetIds.length) {
+      assetIds = await assetManager.fetchAssetIds(...args);
+    }
+
+    activity.assets.large_image = assetIds[0] ?? activity.assets.large_image;
+  } catch (e) {
+    console.error("[RichPresence] Asset resolution failed:", e);
+  }
+
+  return activity;
+}
+
+async function checkState() {
+  const enabled = Boolean(storage.hiddenSettings?.enabled);
+  if (enabled !== lastState) {
+    lastState = enabled;
+
+    if (enabled) {
+      const activity = await resolveAsset({ ...baseActivity });
+      setActivity(activity);
+    } else {
+      setActivity(null);
+    }
   }
 }
 
+export function startRichPresence() {
+  lastState = Boolean(storage.hiddenSettings?.enabled);
+  if (lastState) {
+    resolveAsset({ ...baseActivity }).then((act) => setActivity(act));
+  }
+  interval = setInterval(checkState, 1000);
+}
+
 export function stopRichPresence() {
+  if (interval) {
+    clearInterval(interval);
+    interval = null;
+  }
   setActivity(null);
 }
